7d920c624b90e782886560a3b72613a8bfd2ece5
UgwayK <nuang0530@naver.com>
2023-08-09 13:49:38 +0900

    🔨 main 작업 진행
    - CONTENT_INFO 데이터 불러오는 코드 추가

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
+from datetime import datetime
 
 from utils import set_background_main
 
 # 기본 배경 설정
 set_background_main()
+
+# 초기값 설정
+TODAY_DATE = str(datetime.today().year) + str(datetime.today().month).zfill(2) + str(datetime.today().day).zfill(2)
+if 'CONTENT_INFO' not in st.session_state: # 자막 챌린지 기록 불러오기
+    st.session_state['CONTENT_INFO'] = pd.read_csv(f'./static/data/content_info_{TODAY_DATE}.csv')
+
